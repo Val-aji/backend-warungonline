e5c46a0b301d905cd4c6e9157bbc79c0de339f61
Nouval <ajinopal80@gmail.com>
2023-03-03 00:29:36 +0700

    push awal

--- config/database.js
+++ config/database.js
@@ -1,7 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("warungonline", "root", "", {
-    host: "localhost",
+const DB_NAME = process.env.DB_NAME || "warungonline"
+const DB_HOST = process.env.DB_HOST || "localhost"
+const DB_PORT = process.env.DB_PORT || 3000
+const DB_PASSWORD = process.env.DB_PASSWORD || ""
+const DB_USER = process.env.DB_USER || "root"
+
+const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST
     dialect: "mysql"
 })
 
